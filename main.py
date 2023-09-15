dc0e52543f767233957f56cef174e6c89139247d
Goran Šostarko <77921490+gsostarko@users.noreply.github.com>
2023-09-15 21:50:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,48 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, request, jsonify
 import psycopg2
 
 app = Flask(__name__)
 
-# Configure the PostgreSQL database connection
-db_conn = psycopg2.connect(
-    dbname="railway",
-    user="postgres",
-    password="eYl7DP0W10K3DMUH25md",
-    host="containers-us-west-98.railway.app",  # Change this if your database is on a different host
-    port="6807"  # Change this if your PostgreSQL port is different
-)
+# Database connection parameters
+db_params = {
+    'database': 'railway',
+    'user': 'postgres',
+    'password': 'eYl7DP0W10K3DMUH25md',
+    'host': 'containers-us-west-98.railway.app',  # e.g., localhost or IP address
+    'port': '6807'   # e.g., 5432
+}
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        user_input = request.form.get("user_input")
-        if user_input:
-            # Save the user input to the database
-            cursor = db_conn.cursor()
-            cursor.execute("INSERT INTO data (timestamp) VALUES (%s)", (user_input,))
-            db_conn.commit()
-            cursor.close()
-            return redirect(url_for("index"))
-    return render_template("index.html")
+# Create a table to store data in PostgreSQL
+def create_table():
+    conn = psycopg2.connect(**db_params)
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS data (
+            id SERIAL PRIMARY KEY,
+            timestamp TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/upload', methods=['POST'])
+def upload_data():
+    try:
+        data = request.json.get('data')  # Assuming JSON data
+        if data is None:
+            return jsonify({"message": "No data provided"}), 400
+
+        # Save data to the PostgreSQL database
+        conn = psycopg2.connect(**db_params)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO data (timestamp) VALUES (%s)", (data,))
+        conn.commit()
+        conn.close()
+
+        return jsonify({"message": "Data saved successfully"}), 200
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
+
+if __name__ == '__main__':
+    create_table()  # Create the table if it doesn't exist
+    app.run(host='containers-us-west-98.railway.app', port=6807)
